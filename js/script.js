8d7f6a2ec102d2959acfaa5128c1bd1b3eb3b2b8
centevo <144074037+centevo@users.noreply.github.com>
2023-09-05 09:23:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,158 +1,132 @@
-class World {
-  renderer;
-  scene;
-  camera;
-  molecule;
+var PARTICLE_NUM = 500;
+var PARTICLE_BASE_RADIUS = 0.5;
+var FL = 500;
+var DEFAULT_SPEED = 2;
+var BOOST_SPEED = 300;
 
-  constructor() {
-    this.build();
+var canvas;
+var canvasWidth, canvasHeight;
+var context;
+var centerX, centerY;
+var mouseX, mouseY;
+var speed = DEFAULT_SPEED;
+var targetSpeed = DEFAULT_SPEED;
+var particles = [];
+
+window.addEventListener('load', function() {
+    canvas = document.getElementById('c');
     
-    window.addEventListener('resize', this.resize.bind(this))
-
-    this.animate = this.animate.bind(this);
-    this.animate();
-  }
-
-  build() {
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    this.camera.position.z = 3;
-
-    this.renderer = new THREE.WebGLRenderer({
-      alpha:true,
-      antialias:true
-    });
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
+    var resize = function() {
+        canvasWidth  = canvas.width = window.innerWidth;
+        canvasHeight = canvas.height = window.innerHeight;
+        centerX = canvasWidth * 0.5;
+        centerY = canvasHeight * 0.5;
+        context = canvas.getContext('2d');
+        context.fillStyle = 'rgb(255, 255, 255)';
+    };
     
-    this.molecule = new Molecule()
-    this.scene.add(this.molecule);
-  }
-
-  resize() {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    this.camera.aspect = w / h;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(w, h);
-  }
-
-  animate() {
-    requestAnimationFrame(this.animate);
+    document.addEventListener('resize', resize);
+    resize();
     
-    const time = performance.now() * 0.001;
-
-    this.molecule.animate(time);
+    mouseX = centerX;
+    mouseY = centerY;
     
-
-    this.renderer.render(this.scene, this.camera);
-  }
-}
-
-class Molecule extends THREE.Object3D{
-  material;
-  geometry;
-  mesh;
-  radius = 1.5;
-  detail = 40;
-  particleSizeMin = 0.01;
-  particleSizeMax = 0.08;
-
-  constructor() {
-    super();
-    
-    this.build();
-  }
-
-  build() {
-    
-    this.dot()
-    
-    this.geometry = new THREE.IcosahedronBufferGeometry(1, this.detail);
-
-    this.material = new THREE.PointsMaterial( {
-      map:this.dot(),
-      blending: THREE.AdditiveBlending,
-      color:0x101A88,
-      depthTest:false
-    } )
-
-    this.setupShader(this.material)
-
-    this.mesh = new THREE.Points(this.geometry, this.material);
-    this.add(this.mesh);
-  }
-  
-  dot(size = 32, color = "#FFFFFF"){
-    const sizeH = size * 0.5;
-    
-    const canvas = document.createElement('canvas')
-    canvas.width = canvas.height = size
-    
-    const ctx = canvas.getContext('2d')
-    
-    const circle = new Path2D()
-    circle.arc(sizeH, sizeH, sizeH, 0, 2 * Math.PI);
-    
-    ctx.fillStyle = color;
-    ctx.fill(circle);
-    
-    // debug canvas
-    // canvas.style.position = "fixed"
-    // canvas.style.top = 0
-    // canvas.style.left = 0
-    // document.body.appendChild(canvas)
-    
-    return new THREE.CanvasTexture(canvas)
-  }
-
-  setupShader(material){
-    material.onBeforeCompile = ( shader ) => {
-      shader.uniforms.time = { value: 0 }
-      shader.uniforms.radius = { value: this.radius }
-      shader.uniforms.particleSizeMin = { value: this.particleSizeMin }
-      shader.uniforms.particleSizeMax = { value: this.particleSizeMax }
-      shader.vertexShader = 'uniform float particleSizeMax;\n' + shader.vertexShader;
-      shader.vertexShader = 'uniform float particleSizeMin;\n' + shader.vertexShader;
-      shader.vertexShader = 'uniform float radius;\n' + shader.vertexShader;
-      shader.vertexShader = 'uniform float time;\n' + shader.vertexShader;
-      shader.vertexShader = document.getElementById("webgl-noise").textContent + "\n" + shader.vertexShader;
-      shader.vertexShader = shader.vertexShader.replace(
-        '#include <begin_vertex>',
-        `
-          vec3 p = position;
-          float n = snoise( vec3( p.x*.6 + time*0.2, p.y*0.4 + time*0.3, p.z*.2 + time*0.2) );
-          p += n *0.4;
-
-          // constrain to sphere radius
-          float l = radius / length(p);
-          p *= l;
-          float s = mix(particleSizeMin, particleSizeMax, n);
-          vec3 transformed = vec3( p.x, p.y, p.z );
-        `
-      )
-      shader.vertexShader = shader.vertexShader.replace(
-        'gl_PointSize = size;',
-        'gl_PointSize = s;'
-      )
-
-      material.userData.shader = shader;
-
+    for (var i = 0, p; i < PARTICLE_NUM; i++) {
+        particles[i] = randomizeParticle(new Particle());
+        particles[i].z -= 500 * Math.random();
     }
-  }
+    
+    document.addEventListener('mousemove', function(e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }, false);
+    
+    document.addEventListener('mousedown', function(e) {
+        targetSpeed = BOOST_SPEED;
+    }, false);
+    
+    document.addEventListener('mouseup', function(d) {
+        targetSpeed = DEFAULT_SPEED;
+    }, false);
+    
+    setInterval(loop, 1000 / 60);
+}, false);
 
-  animate(time) {
-    this.mesh.rotation.set(0, time * 0.2, 0);
-    if(this.material.userData.shader)
-      this.material.userData.shader.uniforms.time.value = time;
-  }
-
+function loop() {
+    context.save();
+    context.fillStyle = 'rgb(0, 0, 0)';
+    context.fillRect(0, 0, canvasWidth, canvasHeight);
+    context.restore();
+    
+    speed += (targetSpeed - speed) * 0.01;
+    
+    var p;
+    var cx, cy;
+    var rx, ry;
+    var f, x, y, r;
+    var pf, px, py, pr;
+    var a, a1, a2;
+    
+    var halfPi = Math.PI * 0.5;
+    var atan2  = Math.atan2;
+    var cos    = Math.cos;
+    var sin    = Math.sin;
+    
+    context.beginPath();
+    for (var i = 0; i < PARTICLE_NUM; i++) {
+        p = particles[i];
+        
+        p.pastZ = p.z;
+        p.z -= speed;
+        
+        if (p.z <= 0) {
+            randomizeParticle(p);
+            continue;
+        }
+        
+        cx = centerX - (mouseX - centerX) * 1.25;
+        cy = centerY - (mouseY - centerY) * 1.25;
+        
+        rx = p.x - cx;
+        ry = p.y - cy;
+        
+        f = FL / p.z;
+        x = cx + rx * f;
+        y = cy + ry * f;
+        r = PARTICLE_BASE_RADIUS * f;
+        
+        pf = FL / p.pastZ;
+        px = cx + rx * pf;
+        py = cy + ry * pf;
+        pr = PARTICLE_BASE_RADIUS * pf;
+        
+        a  = atan2(py - y, px - x);
+        a1 = a + halfPi;
+        a2 = a - halfPi;
+        
+        context.moveTo(px + pr * cos(a1), py + pr * sin(a1));
+        context.arc(px, py, pr, a1, a2, true);
+        context.lineTo(x + r * cos(a2), y + r * sin(a2));
+        context.arc(x, y, r, a2, a1, true);
+        context.closePath();
+    }
+    context.fill();
 }
 
-new World();
+function randomizeParticle(p) {
+    p.x = Math.random() * canvasWidth;
+    p.y = Math.random() * canvasHeight;
+    p.z = Math.random() * 1500 + 500;
+    return p;
+}
+
+
+/**
+ * Particle
+ */
+function Particle(x, y, z) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
+    this.pastZ = 0;
+}
