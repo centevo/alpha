e4265400a37e20473750be70772e9a1583239c17
centevo <144074037+centevo@users.noreply.github.com>
2023-12-07 06:26:15 +0100

    kk

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,8 @@
-// Preload images
-const preloadFonts = (id) => {
+/**
+ * Preload fonts
+ * @param {String} id
+ */
+const preloadFonts = id => {
     return new Promise((resolve) => {
         WebFont.load({
             typekit: {
@@ -10,6 +13,19 @@ const preloadFonts = (id) => {
     });
 };
 
+/**
+ * Gets the height of an element without counting with the padding
+ * @param {Element} el
+ */
+const getHeight = el => {
+    const computedStyle = getComputedStyle(el);
+
+    let elementHeight = el.clientHeight;  // height with padding
+    elementHeight -= parseFloat(computedStyle.paddingTop) + parseFloat(computedStyle.paddingBottom);
+    return elementHeight;
+}
+
 export {
-    preloadFonts
+    preloadFonts,
+    getHeight
 };
